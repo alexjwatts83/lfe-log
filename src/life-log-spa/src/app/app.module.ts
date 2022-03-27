d2075bf74f12fb1479742ba6389cc4c024882178
Alex Watts <alexjwatts83@gmail.com>
2022-03-28 08:49:14 +1100

    Added quick actions

--- src/life-log-spa/src/app/app.module.ts
+++ src/life-log-spa/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { QuickActionsComponent } from './quick-actions/quick-actions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    QuickActionsComponent
   ],
   imports: [
     BrowserModule,
