28270d5bd1d86204dcfb56e71945586239f1d20a
Alex Watts <alexjwatts83@gmail.com>
2022-04-01 07:57:18 +1100

    Added header component

--- src/life-log-spa/src/app/app.module.ts
+++ src/life-log-spa/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { QuickActionsComponent } from './quick-actions/quick-actions.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    QuickActionsComponent
+    QuickActionsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
