444e46893842e83f48ca3968aa1745b5627d78f8
Alex Watts <alexjwatts83@gmail.com>
2022-04-11 06:50:13 +1000

    wip quick actions services

--- src/life-log-spa/src/app/quick-actions/quick-actions.component.ts
+++ src/life-log-spa/src/app/quick-actions/quick-actions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import { QuickActions } from './models';
+import { QuickActionsService } from './services/quick-actions.service';
 
 @Component({
   selector: 'app-quick-actions',
@@ -12,7 +13,7 @@ export class QuickActionsComponent implements OnInit {
   faMinus = faMinus;
   quickActions: QuickActions[] = [];
 
-  constructor() { }
+  constructor(private quickActionsService: QuickActionsService) { }
 
   ngOnInit(): void {
   }
