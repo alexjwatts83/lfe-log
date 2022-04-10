b558ba2b7b71d33f345f1927dc404f532e97b332
Alex Watts <alexjwatts83@gmail.com>
2022-04-11 06:52:59 +1000

    more quick services default values

--- src/life-log-spa/src/app/quick-actions/services/quick-actions.service.ts
+++ src/life-log-spa/src/app/quick-actions/services/quick-actions.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { QuickActions, QuickActionsAction } from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuickActionsService {
+  private data: QuickActions[] = [];
+  private _goals$ = new BehaviorSubject<QuickActions[]>(this.data);
 
-  constructor() { }
+  constructor() { 
+    this.data = [
+      {
+        groupTitle: 'Medical',
+        actions: [
+          {
+            defaultValue: 2,
+            name: 'Panandol'
+          } as QuickActionsAction<number>
+        ]
+      } as QuickActions
+    ]
+  }
 }
